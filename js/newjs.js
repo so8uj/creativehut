808a8bfd9235f55865acb0dd5accace0531dcdb8
developersobuj <developersobuj@gmail.com>
2022-07-06 00:20:30 +0600

    new svg added

--- js/newjs.js
+++ js/newjs.js
@@ -4,7 +4,7 @@ if ($('.text-slider').length == 1) {
 var typed_strings = $('.text-slider-items').text();
 	var typed = new Typed('.text-slider', {
 		strings: typed_strings.split(','),
-		typeSpeed: 80,
+		typeSpeed: 100,
 		loop: true,
 		backDelay: 1100,
 		backSpeed: 30
